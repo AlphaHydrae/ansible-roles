c1c2b1a5449e2c007f4f1bdcda6960195ec28a41
AlphaHydrae <hydrae.alpha@gmail.com>
2013-05-26 18:38:53 +0200

    Cleaned tasks.

--- webservers/web-firewalld.sh
+++
@@ -1,8 +0,0 @@
-
-if [ ! -e /web/.firewalld ]; then
-  firewall-cmd --add-service=http
-  firewall-cmd --permanent --add-service=http
-  firewall-cmd --add-service=https
-  firewall-cmd --permanent --add-service=https
-  touch /web/.firewalld
-fi

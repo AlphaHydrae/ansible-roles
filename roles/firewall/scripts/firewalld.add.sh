2d6f03cec52d0fef55a5c98b2a34210f57a4705f
Simon Oulevay <simon.oulevay@gmail.com>
2015-01-09 15:15:43 +0100

    Updated firewall, server, mail-server and postgresql-server roles.

--- roles/firewall/scripts/firewalld.add.sh
+++
@@ -1,9 +0,0 @@
-
-TYPE=$1
-ACTION=$2
-
-if [ ! -e /ansible/firewalld.${TYPE}.${ACTION} ]; then
-  firewall-cmd --zone=public --add-${TYPE}=${ACTION}
-  firewall-cmd --zone=public --permanent --add-${TYPE}=${ACTION}
-  mkdir -p /ansible && touch /ansible/firewalld.${TYPE}.${ACTION}
-fi
